21dbdab8dac903179e8d17677446310882d95ece
Bhavani_CH <64600248+bhavani6353@users.noreply.github.com>
2022-03-12 21:35:08 +0530

    Create 26)Best Time to Buy and Sell Stock II.java

---
+++ 26)Best Time to Buy and Sell Stock II.java
@@ -0,0 +1,72 @@
+/*
+You are given an integer array prices where prices[i] is the price of a given stock on the ith day.
+
+On each day, you may decide to buy and/or sell the stock. You can only hold at most one share of the stock at any time. However, you can buy it then immediately sell it on the same day.
+
+Find and return the maximum profit you can achieve.
+
+ 
+
+Example 1:
+
+Input: prices = [7,1,5,3,6,4]
+Output: 7
+Explanation: Buy on day 2 (price = 1) and sell on day 3 (price = 5), profit = 5-1 = 4.
+Then buy on day 4 (price = 3) and sell on day 5 (price = 6), profit = 6-3 = 3.
+Total profit is 4 + 3 = 7.
+Example 2:
+
+Input: prices = [1,2,3,4,5]
+Output: 4
+Explanation: Buy on day 1 (price = 1) and sell on day 5 (price = 5), profit = 5-1 = 4.
+Total profit is 4.
+Example 3:
+
+Input: prices = [7,6,4,3,1]
+Output: 0
+Explanation: There is no way to make a positive profit, so we never buy the stock to achieve the maximum profit of 0.
+*/
+
+
+
+// method -1
+
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxprofit = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1])
+                maxprofit += prices[i] - prices[i - 1];
+        }
+        return maxprofit;
+    }
+}
+
+
+
+
+// method -2
+
+
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int dp[]=new int[prices.length];
+        int n=prices.length;
+        for(int i=0;i<n;i++) dp[i]=0;
+        int ma=prices[n-1],mc=prices[0];
+        for(int i=n-2;i>=0;i--){
+            if(prices[i]>ma) ma=prices[i];
+            dp[i]=Math.max(dp[i+1],ma-prices[i]);
+        }
+        for(int i=1;i<n;i++){
+            if(prices[i]<mc)mc=prices[i];
+            dp[i]=Math.max(dp[i-1],dp[i]+prices[i]-mc);
+        }
+        return dp[n-1];
+    }
+}
+
+

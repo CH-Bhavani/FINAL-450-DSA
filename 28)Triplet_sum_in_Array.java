022e3a6e64280ea33f45a650be5e3a60da2df5c4
Bhavani_CH <64600248+bhavani6353@users.noreply.github.com>
2022-03-13 20:01:17 +0530

    Create 28)Triplet_sum_in_Array.java

---
+++ 28)Triplet_sum_in_Array.java
@@ -0,0 +1,138 @@
+/*
+Given an array arr of size n and an integer X. Find if there's a triplet in the array which sums up to the given integer X.
+
+
+Example 1:
+
+Input:
+n = 6, X = 13
+arr[] = [1 4 45 6 10 8]
+Output:
+1
+Explanation:
+The triplet {1, 4, 8} in 
+the array sums up to 13.
+Example 2:
+
+Input:
+n = 5, X = 10
+arr[] = [1 2 4 3 6]
+Output:
+1
+Explanation:
+The triplet {1, 3, 6} in 
+the array sums up to 10.
+
+*/
+
+
+
+
+
+
+// method -1
+
+class FindTriplet {
+
+    // returns true if there is triplet with sum equal
+    // to 'sum' present in A[]. Also, prints the triplet
+    boolean find3Numbers(int A[], int arr_size, int sum)
+    {
+        int l, r;
+
+        // Fix the first element as A[i]
+        for (int i = 0; i < arr_size - 2; i++) {
+
+            // Fix the second element as A[j]
+            for (int j = i + 1; j < arr_size - 1; j++) {
+
+                // Now look for the third number
+                for (int k = j + 1; k < arr_size; k++) {
+                    if (A[i] + A[j] + A[k] == sum) {
+                        System.out.print("Triplet is " + A[i] + ", " + A[j] + ", " + A[k]);
+                        return true;
+                    }
+                }
+            }
+        }
+
+        // If we reach here, then no triplet was found
+        return false;
+    }
+  
+  
+  
+  //method-2
+  
+  
+  
+  class FindTriplet {
+
+    // returns true if there is triplet with sum equal
+    // to 'sum' present in A[]. Also, prints the triplet
+    boolean find3Numbers(int A[], int arr_size, int sum)
+    {
+        int l, r;
+
+        /* Sort the elements */
+        Arrays.sort(A);
+
+        /* Now fix the first element one by one and find the
+           other two elements */
+        for (int i = 0; i < arr_size - 2; i++) {
+
+            // To find the other two elements, start two index variables
+            // from two corners of the array and move them toward each
+            // other
+            l = i + 1; // index of the first element in the remaining elements
+            r = arr_size - 1; // index of the last element
+            while (l < r) {
+                if (A[i] + A[l] + A[r] == sum) {
+                    System.out.print("Triplet is " + A[i] + ", " + A[l] + ", " + A[r]);
+                    return true;
+                }
+                else if (A[i] + A[l] + A[r] < sum)
+                    l++;
+
+                else // A[i] + A[l] + A[r] > sum
+                    r--;
+            }
+        }
+
+        // If we reach here, then no triplet was found
+        return false;
+    }
+    
+   // method-3 
+    
+    class GFG {
+
+    // returns true if there is triplet
+    // with sum equal to 'sum' present
+    // in A[]. Also, prints the triplet
+    static boolean find3Numbers(int A[],
+                                int arr_size, int sum)
+    {
+        // Fix the first element as A[i]
+        for (int i = 0; i < arr_size - 2; i++) {
+
+            // Find pair in subarray A[i+1..n-1]
+            // with sum equal to sum - A[i]
+            HashSet<Integer> s = new HashSet<Integer>();
+            int curr_sum = sum - A[i];
+            for (int j = i + 1; j < arr_size; j++) 
+            {
+                if (s.contains(curr_sum - A[j])) 
+                {
+                    System.out.printf("Triplet is %d, 
+                                        %d, %d", A[i],
+                                      A[j], curr_sum - A[j]);
+                    return true;
+                }
+                s.add(A[j]);
+            }
+        }
+
+        // If we reach here, then no triplet was found
+        return false;
+    }
